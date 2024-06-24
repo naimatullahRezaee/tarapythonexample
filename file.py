2472b69edb1012603a007c2a7c8181e9cc3a1f28
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-06-24 18:37:35 +0430

    file processing

--- file.py
+++ file.py
@@ -2,3 +2,41 @@
 
 
 
+# file = open('new.txt', "r")
+
+# print( file.read())
+
+# file.close()
+
+# f = open('new.txt', "a")
+
+# f.write(' ali \n')
+
+# print(f)
+
+# f.close()
+
+# file = open('new.txt', "r")
+
+# print( file.read())
+
+# file.close()
+
+
+
+
+# with open('old.txt','a') as a:
+#     a.write(' karima \n') 
+
+
+# with open('old.txt','r') as file:
+#     f = file.readlines()
+
+#     for i in f:
+#         print(i)
+    
+
+open('file.txt', 'x' )
+
+print('file created')
+
